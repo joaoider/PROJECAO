438a20a1f873f99d98e7f6b5a7b7b10bcc2590a6
joaoider <joaoider>
2025-01-07 18:17:46 +0000

    geral dd

---
+++ Geral/Geral DD/DD_configuracoes.py
@@ -0,0 +1,12 @@
+print('configuracoes.py iniciado')
+
+freq = 'D' # frequencia dos dados Diário
+horizon = 365 # dias futuros para previsao
+variaveis_futuras = ['dayofweek', 'monthofyear', 'dia_das_maes', 'dia_dos_pais', 'dia_dos_namorados', 'halloween', 'black_friday', 'natal', 'copa_do_mundo', 'eleicoes', 'dia_do_trabalhador', 'dia_de_finados', 'confraternizacao_universal', 'independencia_do_brasil', 'nossa_senhora_aparecida', 'proclamacao_da_republica', 'sexta_feira_santa', 'pascoa', 'carnaval', 'covid', 'liquidacao']
+variaveis_historicas = ['QLF', 'ROL', 'CPV']
+# marcas
+marca = 'DD'    # 'JJ', 'DD', 'LL'
+
+data_inicio_base = '2018-01-01'
+
+print('configuracoes.py finalizado')
